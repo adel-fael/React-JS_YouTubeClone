112d88cbef65e6af470a9a2f5bca4fe424fe0188
Adel-Fael <new19.adel@gmail.com>
2022-10-09 20:44:51 +0300

    some failed fixes

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -2,12 +2,15 @@ import axios from 'axios'
 
 export const BASE_URL = 'https://youtube-v31.p.rapidapi.com'
 
+//! this is wrong hide your KEY
+const RAPID_API_KEY = '6c2056a586msh5d5ea533df733e9p18e7e2jsn2eb5fb53e406'
+
 const options = {
   params: {
     maxResults: 50,
   },
   headers: {
-    'X-RapidAPI-Key': '6c2056a586msh5d5ea533df733e9p18e7e2jsn2eb5fb53e406',
+    'X-RapidAPI-Key': RAPID_API_KEY,
     'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
 }
